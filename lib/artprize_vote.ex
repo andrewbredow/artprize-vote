c1c19ac4aa8754e85c9b7703df327eddfe29725b
Andrew Bredow <bredow@gmail.com>
2015-03-16 22:36:09 -0400

    Add guard clause for location

--- lib/artprize_vote.ex
+++ lib/artprize_vote.ex
@@ -1,5 +1,11 @@
 defmodule ArtprizeVote do
-  def add_vote({user_id, entry_id}, votes) when is_integer(user_id) and is_integer(entry_id) do
+  # -85.726469,42.906554,-85.55457,43.01148
+  def add_vote(_, _, {lat, lng}, _)
+    when not lat in -85.726469..-85.55457 or not lng in 42.906554..43.01148 do
+      {:err, "You must be in Grand Rapids to vote."}
+  end
+
+  def add_vote(user_id, entry_id, _, votes) when is_integer(user_id) and is_integer(entry_id) do
     votes = votes ++ [{user_id, entry_id}]
     {:ok, votes}
   end
