635c1f72b49a4fc3ebe62bba2343b6c1d9ab369b
Andrew Bredow <bredow@gmail.com>
2015-03-16 22:49:01 -0400

    Formatting

--- lib/artprize_vote.ex
+++ lib/artprize_vote.ex
@@ -1,11 +1,12 @@
 defmodule ArtprizeVote do
   # -85.726469,42.906554,-85.55457,43.01148
   def add_vote(_, _, {lat, lng}, _)
-    when not lat in -85.726469..-85.55457 or not lng in 42.906554..43.01148 do
-      {:err, "You must be in Grand Rapids to vote."}
+  when not lat in -85.726469..-85.55457 or not lng in 42.906554..43.01148 do
+    {:err, "You must be in Grand Rapids to vote."}
   end
 
-  def add_vote(user_id, entry_id, _, votes) when is_integer(user_id) and is_integer(entry_id) do
+  def add_vote(user_id, entry_id, _, votes)
+  when is_integer(user_id) and is_integer(entry_id) do
     votes = votes ++ [{user_id, entry_id}]
     {:ok, votes}
   end
