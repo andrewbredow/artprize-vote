b486cc424fd84657dbe1163b738ca6e11e1225c7
Andrew Bredow <bredow@gmail.com>
2015-03-16 22:09:08 -0400

    Add guard clause for integer IDs

--- lib/artprize_vote.ex
+++ lib/artprize_vote.ex
@@ -1,5 +1,5 @@
 defmodule ArtprizeVote do
-  def add_vote({user_id, entry_id}, votes) do
+  def add_vote({user_id, entry_id}, votes) when is_integer(user_id) and is_integer(entry_id) do
     votes = votes ++ [{user_id, entry_id}]
     {:ok, votes}
   end
