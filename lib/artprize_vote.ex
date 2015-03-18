d014fa36bc55646c6d6adb29f3fe937da2c5da6c
Andrew Bredow <bredow@gmail.com>
2015-03-17 21:26:55 -0400

    Add ability to request the current winner from the server

--- lib/artprize_vote.ex
+++ lib/artprize_vote.ex
@@ -10,8 +10,8 @@ defmodule ArtprizeVote do
         {_, _, votes} = result
         send sender, result
         listen(votes)
-      {sender, :get_votes} ->
-        send sender, {:ok, votes}
+      {sender, :get_winner} ->
+        send sender, {:ok, calculate_winner(votes)}
         listen(votes)
       {sender, _} ->
         send sender, {:wtf}
@@ -29,5 +29,25 @@ defmodule ArtprizeVote do
   when is_integer(user_id) and is_integer(entry_id) do
     votes = votes ++ [{user_id, entry_id}]
     {:ok, "Successfully voted for entry, #{entry_id}.", votes}
+  end
+
+  def calculate_winner(votes) do
+    unique_votes = unique_by_voter_and_entry(votes)
+    summed_entries = sum_per_entry(unique_votes, %{})
+    ordered_entries = order_by_votes(summed_entries)
+    Enum.at(ordered_entries, 0, %{})
+  end
+
+  defp unique_by_voter_and_entry(votes), do: Enum.uniq votes
+
+  defp sum_per_entry([], sums), do: sums
+  defp sum_per_entry([{_, entry_id}|tail], sums) do
+    initial_value = Dict.get(sums, entry_id, 0)
+    sums = Dict.put(sums, entry_id, initial_value + 1)
+    sum_per_entry(tail, sums)
+  end
+
+  defp order_by_votes(sums) do
+    Enum.sort(sums, fn {_, sum_1}, {_, sum_2} -> sum_1 > sum_2 end)
   end
 end
